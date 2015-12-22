7c4a5318a7a6274aaeb258c33b40d743b47c6a40
IMIDSAC <imidsac@hotmail.fr>
2015-12-22 17:49:55 +0000

    Add deux atbs dans table images.

--- db/migrate/20151218102835_create_images.rb
+++ db/migrate/20151218102835_create_images.rb
@@ -1,6 +1,8 @@
 class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
+      t.string :image_title
+      t.text :description
       t.references :user, index: true
 
       t.timestamps null: false
