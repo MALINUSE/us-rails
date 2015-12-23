1675586e467766496dc2b4a392f44b86ee741283
IMIDSAC <imidsac@hotmail.fr>
2015-12-23 09:16:42 +0000

    precompile delete.

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,5 @@ module UsRails
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
-
   end
 end
