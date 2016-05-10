d38bc2c512226c092462d9470b3a9937eafc7d94
IMIDSAC <imidsac@hotmail.fr>
2016-05-10 16:33:57 +0000

    Correction for deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
+set :repo_url, 'git@github.com:MALINUSE/us-rails.git'
+set :stages, %w(production staging)
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
@@ -26,7 +25,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
