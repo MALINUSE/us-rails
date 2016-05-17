a4f702eaf3074d538d09bb5dc3df2354451a2da8
IMIDSAC <imidsac@hotmail.fr>
2016-05-17 18:26:11 +0000

    Update database.

--- db/migrate/20160510150234_devise_create_users.rb
+++ db/migrate/20160510150234_devise_create_users.rb
@@ -31,6 +31,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.datetime :locked_at
 
 
+      t.string   :role
+
       t.timestamps null: false
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20160516110207) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string "role"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
