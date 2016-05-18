74f6539507bbb01f9867d401d9a21a1252e65011
IMIDSAC <imidsac@hotmail.fr>
2016-05-18 09:39:36 +0000

    Precompile assets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery-1.7.js )
+Rails.application.config.assets.precompile += %w( admin.css.scss )
+Rails.application.config.assets.precompile += %w( admin.js )
+Rails.application.config.assets.precompile += %w( public.css.scss )
+Rails.application.config.assets.precompile += %w( public.js )
+Rails.application.config.assets.precompile += %w( admin.jpg )
