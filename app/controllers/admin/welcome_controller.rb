7eb29427774413f9213228593c65446b65da730f
IMIDSAC <imidsac@hotmail.fr>
2016-05-19 12:16:29 +0000

    Mise en niveaux.

--- app/controllers/admin/welcome_controller.rb
+++ app/controllers/admin/welcome_controller.rb
@@ -1,5 +1,11 @@
 class Admin::WelcomeController < Admin::ApplicationController
   def index
-
+    if current_user.role == "superAdmin"
+      @users_count = User.all.count
+      @posts_count = Post.all.count
+    else
+      @users_count= nil
+      @posts_count = current_user.posts.all.count
+    end
   end
 end
