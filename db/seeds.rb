b2e56d964d6312a893095e3cd07893bf2994b6a7
IMIDSAC <imidsac@hotmail.fr>
2016-05-24 17:51:43 +0000

    Update seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,37 @@ User.create!([
 puts "===> Sample date have been set in DB  Users!"
 
 ####################################################### END DATA COMMUN ################################################
+
+Subject.delete_all
+Subject.create!([
+                    {name: "Présentation", position: 1, visible: true},
+                    {name: "Aide et Soutien", position: 2, visible: true}
+
+                ])
+
+puts "===> Sample date have been set in DB  Subjects!"
+
+Page.delete_all
+Page.create!([
+                 {subject_id: 1, name: "Malinuse se présente", position: 1, visible: true},
+                 {subject_id: 1, name: "Vision et Mission de Malinuse", position: 2, visible: true},
+                 {subject_id: 1, name: "Mot du PDG", position: 3, visible: true},
+
+                 {subject_id: 2, name: "Annuaire", position: 4, visible: true}
+
+             ])
+
+puts "===> Sample date have been set in DB  Pages!"
+
+Section.delete_all
+Section.create!([
+                    {page_id: 1, name: "Malinuse se présente Section 1", position: 1, visible: true, content_type: "text", content: "Malinuse se présente Section 1"},
+                    {page_id: 1, name: "Malinuse se présente Section 2", position: 2, visible: true, content_type: "text", content: "Malinuse se présente Section 2"},
+                    {page_id: 1, name: "Malinuse se présente Section 3", position: 3, visible: true, content_type: "text", content: "Malinuse se présente Section 3"},
+
+                    {page_id: 2, name: "Vision et Mission de Malinuse section 1", position: 4, visible: true, content_type: "text", content: "Vision et Mission de Malinuse section 1"},
+                    {page_id: 2, name: "Vision et Mission de Malinuse section 2", position: 5, visible: true, content_type: "text", content: "Vision et Mission de Malinuse section 2"}
+
+                ])
+
+puts "===> Sample date have been set in DB  Sections!"
