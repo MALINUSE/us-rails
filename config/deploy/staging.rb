d0b9eefb4c4e722d9d394171d5f5742d1922df93
IMIDSAC <imidsac@hotmail.fr>
2016-06-01 10:53:41 +0000

    Correction for deploy.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,7 @@
 
 set :application, 'STAGING_UVERSITE'
 
-server 'malinuse-server2', user: 'imidsac', roles: %w{app db web}, :primary => true
+server 'server-app', user: 'imidsac', roles: %w{app db web}, :primary => true
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
