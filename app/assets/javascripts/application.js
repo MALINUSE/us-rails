cd33a75fe795e32d3d433ab0361b141646c8da2e
IMIDSAC <imidsac@hotmail.fr>
2016-06-01 10:41:42 +0000

    Fixed problem for assets:precompile.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,17 +11,43 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
-//= require prettyphoto-rails
-//= require prettyphoto-rails-dev
 //= require jquery_ujs
-// require turbolinks
-//= require jquery.flexslider-min
-//= require jquery-placeholder
-//= require back-to-top
-//= require main
-//= require map
-//= require jflickrfeed.min
-//= gmaps
-//= gmaps_ujs
-//= require_tree .
+//= require bootstrap-sprockets
+//= require bootstrap-hover-dropdown.min.js
+//= require back-to-top.js
+//= require jquery.placeholder.js
+//= require jquery.prettyPhoto.js
+//= require jquery.flexslider-min.js
+//= require main.js
+//= require pages
+
+
+//= require froala_editor.min.js
+// Include other plugins.
+//= require plugins/align.min.js
+//= require plugins/char_counter.min.js
+//= require plugins/code_beautifier.min.js
+//= require plugins/code_view.min.js
+//= require plugins/colors.min.js
+//= require plugins/emoticons.min.js
+//= require plugins/entities.min.js
+//= require plugins/file.min.js
+//= require plugins/font_family.min.js
+//= require plugins/font_size.min.js
+//= require plugins/fullscreen.min.js
+//= require plugins/image.min.js
+//= require plugins/image_manager.min.js
+//= require plugins/inline_style.min.js
+//= require plugins/line_breaker.min.js
+//= require plugins/link.min.js
+//= require plugins/lists.min.js
+//= require plugins/paragraph_format.min.js
+//= require plugins/paragraph_style.min.js
+//= require plugins/quote.min.js
+//= require plugins/save.min.js
+//= require plugins/table.min.js
+//= require plugins/url.min.js
+//= require plugins/video.min.js
+
+
+
